eb8b10be35838601e2ce895308a3416981efccb5
Ian Macali <ianmacalinao@gmail.com>
2012-02-29 22:56:26 -0600

    Update for Bukkit R5.

--- src/main/java/me/kalmanolah/ol/OKCommandManager.java
+++ src/main/java/me/kalmanolah/ol/OKCommandManager.java
@@ -6,26 +6,27 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
-public class OKCommandManager
-{
-  private OKmain plugin;
-  private Map<String, CommandExecutor> commands = new Hashtable();
+public class OKCommandManager {
+    private OKmain plugin;
 
-  public OKCommandManager(OKmain instance) {
-    this.plugin = instance;
-  }
+    private Map<String, CommandExecutor> commands = new Hashtable();
 
-  public void addCommand(String label, CommandExecutor executor) {
-    this.commands.put(label, executor);
-  }
-
-  public boolean dispatch(CommandSender sender, Command command, String label, String[] args) {
-    if (!this.commands.containsKey(label)) {
-      return false;
+    public OKCommandManager(OKmain instance) {
+        this.plugin = instance;
     }
-    boolean handled = true;
-    CommandExecutor ce = (CommandExecutor)this.commands.get(label);
-    handled = ce.onCommand(sender, command, label, args);
-    return handled;
-  }
+
+    public void addCommand(String label, CommandExecutor executor) {
+        this.commands.put(label, executor);
+    }
+
+    public boolean dispatch(CommandSender sender, Command command, String label, String[] args) {
+        if (!this.commands.containsKey(label)) {
+            return false;
+        }
+        boolean handled = true;
+        CommandExecutor ce = (CommandExecutor) this.commands.get(label);
+        handled = ce.onCommand(sender, command, label, args);
+        return handled;
+    }
+
 }
--- src/main/java/me/kalmanolah/ol/OKLogFile.java
+++ src/main/java/me/kalmanolah/ol/OKLogFile.java
@@ -6,93 +6,97 @@ import java.io.FileWriter;
 import org.bukkit.Server;
 import org.bukkit.scheduler.BukkitScheduler;
 
-public class OKLogFile
-{
-  private static OKmain plugin;
-  public static String directory = "plugins" + File.separator + OKmain.name;
-  public static String subdirectory = "plugins" + File.separator + OKmain.name + File.separator + "logs";
-  public static String subsubdirectory = subdirectory + File.separator + OKmain.getDate();
-  static File chatfile = new File(subdirectory + File.separator + OKmain.getDate() + File.separator + OKmain.chatlogname + ".txt");
-  static File cmdfile = new File(subdirectory + File.separator + OKmain.getDate() + File.separator + OKmain.cmdlogname + ".txt");
+public class OKLogFile {
+    private static OKmain plugin;
 
-  public OKLogFile(OKmain instance)
-  {
-    plugin = instance;
-  }
+    public static String directory = "plugins" + File.separator + OKmain.name;
 
-  public void logCheck()
-  {
-    new File(directory).mkdir();
-    new File(subdirectory).mkdir();
-    new File(subsubdirectory).mkdir();
-    if (!chatfile.exists()) {
-      try {
-        chatfile.createNewFile();
-      } catch (Exception ex) {
-        ex.printStackTrace();
-      }
-    }
-    if (!cmdfile.exists()) {
-      try {
-        cmdfile.createNewFile();
-      } catch (Exception ex) {
-        ex.printStackTrace();
-      }
-    }
-    prune();
-    plugin.getServer().getScheduler().scheduleSyncRepeatingTask(plugin, new Runnable() {
-      public void run() {
-        OKLogFile.prune();
-      }
-    }
-    , 3600L, 72000L);
-  }
+    public static String subdirectory = "plugins" + File.separator + OKmain.name + File.separator + "logs";
 
-  public static void writeCmd(String args) {
-    subsubdirectory = subdirectory + File.separator + OKmain.getDate();
-    new File(subsubdirectory).mkdir();
-    File file = new File(subsubdirectory + File.separator + OKmain.cmdlogname + ".txt");
-    if (!file.exists())
-      try {
-        file.createNewFile();
-      } catch (Exception ex) {
-        ex.printStackTrace();
-      }
-    try
-    {
-      FileWriter fstream = new FileWriter(file, true);
-      BufferedWriter out = new BufferedWriter(fstream);
-      out.write(OKmain.getDateTime() + " | " + args);
-      out.newLine();
-      out.close();
-    } catch (Exception localException1) {
-    }
-  }
+    public static String subsubdirectory = subdirectory + File.separator + OKmain.getDate();
 
-  public static void writeChat(String args) {
-    subsubdirectory = subdirectory + File.separator + OKmain.getDate();
-    new File(subsubdirectory).mkdir();
-    File file = new File(subsubdirectory + File.separator + OKmain.chatlogname + ".txt");
-    if (!file.exists())
-      try {
-        file.createNewFile();
-      } catch (Exception ex) {
-        ex.printStackTrace();
-      }
-    try
-    {
-      FileWriter fstream = new FileWriter(file, true);
-      BufferedWriter out = new BufferedWriter(fstream);
-      out.write(OKmain.getDateTime() + " | " + args);
-      out.newLine();
-      out.close();
-    } catch (Exception localException1) {
-    }
-  }
+    static File chatfile = new File(subdirectory + File.separator + OKmain.getDate() + File.separator + OKmain.chatlogname + ".txt");
 
-  public static void prune() {
-    File oldfolder = new File(subdirectory + File.separator + OKmain.getPruneDate());
-    if (oldfolder.exists())
-      oldfolder.delete();
-  }
+    static File cmdfile = new File(subdirectory + File.separator + OKmain.getDate() + File.separator + OKmain.cmdlogname + ".txt");
+
+    public OKLogFile(OKmain instance) {
+        plugin = instance;
+    }
+
+    public void logCheck() {
+        new File(directory).mkdir();
+        new File(subdirectory).mkdir();
+        new File(subsubdirectory).mkdir();
+        if (!chatfile.exists()) {
+            try {
+                chatfile.createNewFile();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+        if (!cmdfile.exists()) {
+            try {
+                cmdfile.createNewFile();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+        prune();
+        plugin.getServer().getScheduler().scheduleSyncRepeatingTask(plugin, new Runnable() {
+            public void run() {
+                OKLogFile.prune();
+            }
+
+        }, 3600L, 72000L);
+    }
+
+    public static void writeCmd(String args) {
+        subsubdirectory = subdirectory + File.separator + OKmain.getDate();
+        new File(subsubdirectory).mkdir();
+        File file = new File(subsubdirectory + File.separator + OKmain.cmdlogname + ".txt");
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+        try {
+            FileWriter fstream = new FileWriter(file, true);
+            BufferedWriter out = new BufferedWriter(fstream);
+            out.write(OKmain.getDateTime() + " | " + args);
+            out.newLine();
+            out.close();
+        } catch (Exception localException1) {
+        }
+    }
+
+    public static void writeChat(String args) {
+        subsubdirectory = subdirectory + File.separator + OKmain.getDate();
+        new File(subsubdirectory).mkdir();
+        File file = new File(subsubdirectory + File.separator + OKmain.chatlogname + ".txt");
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+        try {
+            FileWriter fstream = new FileWriter(file, true);
+            BufferedWriter out = new BufferedWriter(fstream);
+            out.write(OKmain.getDateTime() + " | " + args);
+            out.newLine();
+            out.close();
+        } catch (Exception localException1) {
+        }
+    }
+
+    public static void prune() {
+        File oldfolder = new File(subdirectory + File.separator + OKmain.getPruneDate());
+        if (oldfolder.exists()) {
+            oldfolder.delete();
+        }
+    }
+
 }
